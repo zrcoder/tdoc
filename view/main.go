5f80e635e0dd92bfa3d221f4fdc4fc9463a1bf79
zrcoder <love-nankai@163.com>
2024-04-28 22:18:58 +0800

    update fmt

--- view/main.go
+++ view/main.go
@@ -7,9 +7,7 @@ import (
 	"github.com/zrcoder/tdoc/model"
 )
 
-var (
-	ErrStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("#f00"))
-)
+var ErrStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("#f00"))
 
 const (
 	HelpHeight = 4
